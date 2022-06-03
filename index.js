41dfe63901c1f4626c617f8d4d93d42428555a60
SecurID <yannick.kupferschmidt@googlemail.com>
2022-06-03 17:51:36 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
