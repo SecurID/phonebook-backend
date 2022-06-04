92055effa7c7997822861f20cd097536872d1e5e
SecurID <yannick.kupferschmidt@googlemail.com>
2022-06-04 23:18:34 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -23,7 +23,7 @@ const personSchema = new mongoose.Schema({
       minlength: 8,
       validate: {
         validator: function(v) {
-          return /\s\d{2,3}-\d*\s/.test(v);
+          return /\d{2,3}-\d*/.test(v);
         },
         message: props => `${props.value} is not a valid phone number!`
       },
